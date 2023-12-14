f4ff71b1e6a5c436849dcb0f8a8214d0edbbc0cf
Joseph Barnett <joseph-barnett@outlook.com>
2023-12-14 14:03:10 +0000

    refactor api to use pokemon instead of superheros

--- src/pages/SuperheroPage/index.jsx
+++ src/pages/SuperheroPage/index.jsx
@@ -3,30 +3,16 @@ import { SearchForm, SuperheroCard } from '../../components'
 
 function SuperHeroPage() {
 
-  const [search, setSearch] = useState('batman');
+  const [search, setSearch] = useState('Charmander');
   const [showData, setShowData] = useState([])
 
   useEffect(() => {
     async function fetchSuperheroData() {
-      try {   //https://superheroapi.com/api/122096483342157171
-        const response = await fetch(`https://superheroapi.com/api/122096483342157171/search/${search}`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok.');
-        }
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
+        console.log(response)
   
         const data = await response.json();
-        const processedData = data.results.map(superhero => ({
-          name: superhero.name,
-          speed: superhero.powerstats.speed,
-          strength: superhero.powerstats.strength,
-          intelligence: superhero.powerstats.intelligence,
-          imageUrl: superhero.image.url
-        }));
-  
-        setShowData(processedData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+        setShowData(data)
     }
   
     fetchSuperheroData();
@@ -37,7 +23,7 @@ function SuperHeroPage() {
   <>
     <SearchForm onSearch={setSearch}/>
     <div className='heroList'>
-      {showData.map((hero, index) => (<SuperheroCard key={index} hero={hero} />))}
+      <SuperheroCard key={showData.id} hero={showData} />
     </div>
   </>
     
